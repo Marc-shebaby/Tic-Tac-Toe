64348670857b1052cbbe3d31a7e10d7b2f2ff034
Marc Shebaby <marcshababy02@gmail.com>
2022-09-28 23:25:10 +0300

    front end update

--- minimax.js
+++ minimax.js
@@ -1,31 +1,19 @@
-window.addEventListener('load',() =>{
-    
-    const canvas=document.getElementById("canvas");
-    const ctx=canvas.getContext("2d");
-    canvas.height =window.innerHeight;
-    canvas.width =window.innerWidth;
-    ctx.font="75px Ariel";
-    ctx.fillText("Tic-Tac-Toe Game", (canvas.width/2)-315,75);
-    ctx.textAlign="center";
-  
-   
-    
-    ctx.lineWidth=10;;
-    ctx.lineCap="square";
-    ctx.beginPath();
-    ctx.moveTo(600,700);
-    ctx.lineTo(1370,700);
-    
-    ctx.moveTo(600,400);
-    ctx.lineTo(1370,400);
 
-    ctx.moveTo(850,925);
-    ctx.lineTo(850,175);
-    ctx.moveTo(1111,925);
-    ctx.lineTo(1111,175);
    
-    ctx.closePath();
+   
+function aniM(e){
+    const x=e.target.id;
+    const nm=document.getElementById(x);
+    console.log(x);
+    let coor=nm.getBoundingClientRect();
+    console.log(u['x']);
+    console.log(nm.getBoundingClientRect());
+  
     
-    ctx.stroke();
-    
-});
+
+}
+const m= document.querySelectorAll(".mid");
+   for (var i=0; i<m.length; i++){
+    m[i].addEventListener("click",aniM);
+console.log("Koo");
+   }
